cb44e9648d00e893f8a6f15f39d8aa2855d4d1f9
Daeheon <shew1903@gmail.com>
2022-05-13 11:04:07 +0900

    RNN-inmport libraries and training set

--- RNNs.py
+++ RNNs.py
@@ -4,6 +4,20 @@
 #Part1 - Data Preprocessing
 
 #Importing the Libraries
+import numpy as np
+import  matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the training set
+# Only Numnpy array can be the input of neural networks in keras
+dataset_train = pd.read_csv('Google_stock_Price_Train.csv') #Importing csv file
+training_set = dataset_train.iloc[:,1:2].values             #Make Numpt array - .values (index 1)
+print(type(training_set))
+print(training_set)
+
+
+
+
 
 
 #Part 2 - Building the RNN
