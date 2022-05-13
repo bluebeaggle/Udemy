4b0165ace7f523310ee5c6e486acb63316fa0538
Daeheon <shew1903@gmail.com>
2022-05-13 11:31:18 +0900

    RNN-Feature Scaling

--- RNNs.py
+++ RNNs.py
@@ -15,6 +15,19 @@ training_set = dataset_train.iloc[:,1:2].values             #Make Numpt array - 
 print(type(training_set))
 print(training_set)
 
+#Feature Scaling 
+#Recommand Normalisation when Sigmoid Activation Function
+
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range =  (0,1))       #증감 주기는 between 0 and 1
+training_set_scaled = sc.fit_transform(training_set)    #정규화
+
+print(type(training_set_scaled))
+print(training_set_scaled)
+
+#Creating a data structure with 60 timesteps and 1 output
+# 순환 신경망이 기억해야 할 사항을 명시하는 데이터 구조
+
 
 
 
