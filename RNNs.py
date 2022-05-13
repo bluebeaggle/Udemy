0c542e97cbfa720b571e666e3f0ff9e678b96085
Daeheon <shew1903@gmail.com>
2022-05-13 10:50:06 +0900

    RNN

---
+++ RNNs.py
@@ -0,0 +1,13 @@
+# Predict the stock price of Google
+
+
+#Part1 - Data Preprocessing
+
+#Importing the Libraries
+
+
+#Part 2 - Building the RNN
+
+
+#Part 3 - Making the predictions and visualising the results
+
