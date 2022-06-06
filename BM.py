b901b6c64725dc97b1c06d8552191941a4d955f4
Daeheon <shew1903@gmail.com>
2022-06-06 20:07:29 +0900

    BM- Importing the dataset

--- BM.py
+++ BM.py
@@ -7,4 +7,80 @@
 '''
 '''Download Dataset'''
 
+'''전처리 과정 - 
+데이터 불러오기, 훈련세트&데스트세트 준비,사용자수&영화수 Counting,
+사용자를 행으로 영화를 열로 변환, 데이터를 torch.Tensor로 변환'''
+
 """##Importing the libraries"""
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.nn.parallel            # 병렬계산
+import torch.optim as optim         # 옵티마이저
+import torch.utils.data
+from torch.autograd import Variable # 확률적 범위 와 하강
+
+# Importing the dataset
+movies = pd.read_csv(r'C:\Users\Daeheon\Desktop\Daeheon\PRO\All of  DeepLearning\P16-Boltzmann-Machines\Boltzmann_Machines\ml-1m\movies.dat', sep = '::', header = None, engine= 'python', encoding= 'latin-1')
+users = pd.read_csv(r'C:\Users\Daeheon\Desktop\Daeheon\PRO\All of  DeepLearning\P16-Boltzmann-Machines\Boltzmann_Machines\ml-1m\users.dat', sep = '::', header = None, engine= 'python', encoding= 'latin-1')
+ratings = pd.read_csv(r'C:\Users\Daeheon\Desktop\Daeheon\PRO\All of  DeepLearning\P16-Boltzmann-Machines\Boltzmann_Machines\ml-1m\ratings.dat', sep = '::', header = None, engine= 'python', encoding= 'latin-1')
+
+# Preparing the Training set and the test set
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
